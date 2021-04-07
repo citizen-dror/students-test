17671d27f3bc3230f6a14238e8005d43f12f5690
citizen-dror <reshef.dror@gmail.com>
2021-04-07 18:45:33 +0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import StudentsForm from './components/StudetsForm.tsx';
 import StudentsTable from './components/StudentsTable.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <div>
-        <StudentsForm />
-        <StudentsTable />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/form">Students Form</Link>
+              </li>
+              <li>
+                <Link to="/table">Students Table</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/form">
+              <StudentsForm />
+            </Route>
+            <Route path="/table">
+              <StudentsTable />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
+}
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
