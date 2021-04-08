85432344f70639657a622e93107939e7688215cd
citizen-dror <reshef.dror@gmail.com>
2021-04-08 12:01:42 +0300

    students - get city name

--- server/src/components/students/students_dal.js
+++ server/src/components/students/students_dal.js
@@ -2,7 +2,7 @@ const db = require('../../database');
 
 class StudentsDal {
     get = async () => {
-       const qSreing = 'SELECT * FROM students';
+       const qSreing = 'SELECT students.*, cities.city_name FROM students INNER JOIN cities ON (students.city_id  = cities.city_id)';
        const rows = await db.query(qSreing);
        return rows;
     }
