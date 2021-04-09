bf6145fa425f1c427c6831f2206e4c10dc969d3b
citizen-dror <reshef.dror@gmail.com>
2021-04-09 12:38:01 +0300

    add basic morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan')
 const db = require('./database');
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+
 const studetnsRoute = require('./components/students/students_api');
 const citiesRoute = require('./components/cities/cities_api');
 const app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
